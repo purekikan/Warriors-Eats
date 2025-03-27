cf7f76447fcd7bee011c5d6f3bbe6765127e3b46
purekikan <kikansalavia@gmail.com>
2025-03-27 14:14:23 -0400

    Debug Home Search Keyboard Disappear

--- my-project/my-app/src/app/home.jsx
+++ my-project/my-app/src/app/home.jsx
@@ -2,10 +2,9 @@ import { View, Text, ScrollView, FlatList, StatusBar } from 'react-native';
 import * as React from 'react';
 import { Searchbar, PaperProvider } from 'react-native-paper';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import ReviewCard from '../components/reviewCard';
+import { ReviewCard, HomeHeader } from '../components';
 
 const home = () => {
-  const [searchQuery, setSearchQuery] = React.useState('');
   const DATA = [
     {
       id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
@@ -21,50 +20,19 @@ const home = () => {
     },
   ];
   
-  const Item = ({title}) => (
-    <View>
-      <ReviewCard />
-    </View>
-  );
-  
-  const HomeHeader = () => {
-    return (
-      <>
-      <StatusBar backgroundColor="#F8D49B" barStyle="dark-content" />
-        {/* header */}
-        <View className="flex px-4 py-6 bg-[#F8D49B]">
-          <View className="flex justify-between items-start flex-row mb-6">
-            <View>
-              <Text className="text-2xl font-psemibold text-black">
-                Welcome Back Purekikan
-              </Text>
-            </View>
-          </View>
-  
-          <Searchbar
-            placeholder="Search"
-            onChangeText={setSearchQuery}
-            value={searchQuery}
-          />
-        </View>
-      </>
-    )
-  }
-  
   return (
     <SafeAreaProvider>
-      <HomeHeader />
       <SafeAreaView className='flex-1 bg-[#FFF1C2]'>
-          {/* all posts (cards) */}
-          <FlatList className='pt-4'
-            data={DATA}
-            renderItem={({item}) => <Item title={item.title} />}
-            keyExtractor={item => item.id} 
-            ListHeaderComponent = {() => <>
-              <Text>home</Text>
-              <Text>You searched: {searchQuery}</Text>
-            </>}
-            />
+        <HomeHeader />
+        {/* all posts (cards) */}
+        <FlatList className='pt-4'
+          data={DATA}
+          renderItem={({item}) => <ReviewCard title={item.title} />}
+          keyExtractor={item => item.id} 
+          ListHeaderComponent = {() => <>
+            <Text>home</Text>
+          </>}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   )
