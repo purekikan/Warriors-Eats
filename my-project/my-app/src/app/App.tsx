12e52494e524d75707931617ec0bcaf42c37f26c
purekikan <kikansalavia@gmail.com>
2025-03-27 00:54:34 -0400

    Customize bottom navbar

--- my-project/my-app/src/app/App.tsx
+++ my-project/my-app/src/app/App.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
-import App from './index';
 import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
 
-const Tab = createMaterialBottomTabNavigator();
+// const Tab = createMaterialBottomTabNavigator();
 
 const theme = {
   ...DefaultTheme,
@@ -14,28 +13,27 @@ const theme = {
   },
 };
 
-import Create from "./create";
+import CreateRoute from "./create";
+import HomeRoute from './home';
+import RestaurantsRoute from './restaurants';
+import AppRoute from './index';
 import { BottomNavigation, Text } from 'react-native-paper';
-
-const RecentsRoute = () => <Text>Recents</Text>;
-
-const NotificationsRoute = () => <Text>Notifications</Text>;
 
 
 export default function Main() {
     const [index, setIndex] = React.useState(0);
     const [routes] = React.useState([
-        { key: 'app', title: 'Home', focusedIcon: 'heart', unfocusedIcon: 'heart-outline'},
-        { key: 'create', title: 'Create', focusedIcon: 'album' },
-        { key: 'recents', title: 'Recents', focusedIcon: 'history' },
-        { key: 'notifications', title: 'Notifications', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+        { key: 'app', title: 'Test', focusedIcon: 'heart', unfocusedIcon: 'heart-outline'},
+        { key: 'home', title: 'Home', focusedIcon: 'home', unfocusedIcon: 'home-outline' },
+        { key: 'create', title: 'Create', focusedIcon: 'plus-circle', unfocusedIcon: 'plus-circle-outline' },
+        { key: 'restaurants', title: 'Restaurants', focusedIcon: 'food', unfocusedIcon: 'food-outline' },
     ]);
 
     const renderScene = BottomNavigation.SceneMap({
-        app: App,
-        create: Create,
-        recents: RecentsRoute,
-        notifications: NotificationsRoute,
+        app: AppRoute,
+        home: HomeRoute,
+        create: CreateRoute,
+        restaurants: RestaurantsRoute,
     });
     return (
         <PaperProvider theme={theme}>
