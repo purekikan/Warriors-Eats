34c81253672ed010cb9a6565e469449acf2c0148
purekikan <kikansalavia@gmail.com>
2025-03-27 00:39:52 -0400

    adding default bottom navbar

---
+++ my-project/my-app/src/app/create.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const Create = () => {
+  return (
+    <>
+      <View>
+        <Text>create</Text>
+      </View>
+    </>
+  )
+}
+
+export default Create
---
+++ my-project/my-app/src/app/tabs/restaurants.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const restaurants = () => {
+  return (
+    <View>
+      <Text>restaurants</Text>
+    </View>
+  )
+}
+
+export default restaurants
