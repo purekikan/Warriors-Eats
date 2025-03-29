d599a861f9d97bfba988669c24532ea9acba784f
TylerYip1 <tylershcy2024@student.cis.edu.hk>
2025-03-29 15:51:20 -0400

    Add restaurant list with expandable items

---
+++ my-project/my-app/src/data/restaurantLists.js
@@ -0,0 +1,10 @@
+const restaurants = [
+    {
+      id: 1,
+      name: "Watson's Eatery",
+      description: "Watson's is a restaurant in United College. You can go there by following people maps. The food here depends on the day you come, sometimes it is good, but most of the times it is bad. GO AWAY DO NOT EAT HERE."
+    },
+    // Add more restaurants as needed
+  ];
+
+  export default restaurants;
