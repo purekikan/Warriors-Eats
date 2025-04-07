d44d8aff4f89548c3a257b3536f95525427b395b
purekikan <kikansalavia@gmail.com>
2025-04-07 16:33:54 -0400

    image handling

--- my-project/my-app/src/app/home.jsx
+++ my-project/my-app/src/app/home.jsx
@@ -1,63 +1,71 @@
-import { View, Text, ScrollView, FlatList, StatusBar } from 'react-native';
+import { View, ScrollView, FlatList, StatusBar, ActivityIndicator } from 'react-native';
 import * as React from 'react';
-import { Searchbar, PaperProvider } from 'react-native-paper';
+import { PaperProvider } from 'react-native-paper';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { ReviewCard, HomeHeader } from '../components';
 import axios from 'axios';
-const home = () => {
-  const [DATA, setDATA] = React.useState([
-    {
-      id:1,
-      address: "Foo",
-      description: "A decent place to eat",
-      name:"United College"
-    },
-    {
-      id:2,
-      address:"Bar",
-      description:"A so so place to eat",
-      name:"St. Jeromes"
-    },
-    {
-      id: '3',
-      name: 'Third Item',
-    },
-    {
-      id: '4',
-      name: 'Fourth Item',
-    },
-    {
-      id: '5',
-      name: 'Fifth Item', 
-    }
-  ]);
+
+const Home = () => {
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
   React.useEffect(() => {
-          axios.get('http://localhost:3000/eateries')
-            .then(response => {
-              console.log(response.data);
-              setDATA(response.data);
-            })
-            .catch(error => {
-              console.error(error);
-            });
-        }, []);
-  
+    axios.get('http://localhost:3000/reviews')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Error: {error}</Text>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaProvider>
-      <SafeAreaView className='flex-1 bg-[#FFF1C2]'>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF1C2' }}>
+        {/* Add your header here */}
         <HomeHeader />
-        {/* all posts (cards) */}
-        <FlatList className='pt-6 px-5'
-          data={DATA}
-          renderItem={({item}) => <ReviewCard title={item.name} />}
-          keyExtractor={item => item.id} 
-          ItemSeparatorComponent={() => <View style={{ height: 22 }} />} 
+        
+        <FlatList
+          style={{ paddingTop: 24, paddingHorizontal: 20 }}
+          data={data}
+          renderItem={({item}) => (
+            <ReviewCard 
+              key={item.id}
+              title={item.food_name} 
+              image_data={item.image_data?.data} 
+              location={item.location}
+              reviewTitle={item.review_title}
+              reviewDescriptions={item.review_description}
+            />
+          )}
+          keyExtractor={item => item.id.toString()}
+          ItemSeparatorComponent={() => <View style={{ height: 22 }} />}
           contentContainerStyle={{ paddingBottom: 22 }}
         />
       </SafeAreaView>
     </SafeAreaProvider>
-  )
+  );
 }
 
-
-export default home;
+export default Home;
